74606690e3321b9fe14de765d80bb1fb41cc6a7e
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2026-02-21 19:28:52 +0530

    feat(city): wire city routes into v1 router (GRN-74)

--- src/routes/v1/router.ts
+++ src/routes/v1/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { countryRouter } from "./country/country.routes";
 import { stateRouter } from "./state/state.routes";
+import { cityRouter } from "./city/city.routes";
 
 // IMPORT OTHER ROUTES
 
@@ -9,5 +10,6 @@ const routerv1: Router = Router();
 // APPEND API ROUTES
 routerv1.use("/countries", countryRouter);
 routerv1.use("/states", stateRouter);
+routerv1.use("/city", cityRouter);
 
 export default routerv1;
